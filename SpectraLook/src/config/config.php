e768bc100603623574278c02d3af083c58bbcd0b
Roberto <rvega_22@alu.uabcs.mx>
2024-11-30 18:24:50 -0700

    Arreglo de urls

--- SpectraLook/src/config/config.php
+++ SpectraLook/src/config/config.php
@@ -1,7 +1,7 @@
 <?php
     return [
-        'base_url' => 'http://localhost/SpectraLook/public',
-        'assets_url' => 'http://localhost/SpectraLook/public/assets',
+        'base_url' => 'http://localhost/Proyecto-Final/SpectraLook/public',
+        'assets_url' => 'http://localhost/Proyecto-Final/SpectraLook/public/assets',
         'db' => [
             'host' => '127.0.0.1',
             'name' => 'spectralook',
